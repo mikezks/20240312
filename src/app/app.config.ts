4ecf365aa03fcab15269c4198a3924417738c734
mikezks <23002661+mikezks@users.noreply.github.com>
2024-03-12 16:21:17 +0100

    feat: base url token

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { FlightService } from './flight-search/flight.service';
 import { DefaultFlightService } from './flight-search/default-flight.service';
+import { BASE_URL } from './app.token';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -9,6 +10,10 @@ export const appConfig: ApplicationConfig = {
     {
       provide: FlightService,
       useClass: DefaultFlightService
-    }
+    },
+    /* {
+      provide: BASE_URL,
+      useValue: 'this-url-is-not-working'
+    }, */
   ]
 };
---
+++ src/app/app.token.ts
@@ -0,0 +1,5 @@
+import { InjectionToken } from "@angular/core";
+
+export const BASE_URL = new InjectionToken<string>('BASE_URL', {
+  factory: () => 'https://demo.angulararchitects.io/api/'
+});
